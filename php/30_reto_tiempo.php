5550e3b9190dfe0641fa43385751eb3eefb6df93
“nanajero” <nana-jero@hotmail.com>
2016-05-11 07:45:42 -0500

    “php”

--- php/30_reto_tiempo.php
+++ php/30_reto_tiempo.php
@@ -1,62 +1,66 @@
-<html>
+<!DOCTYPE html>
+<html lang="en">
 <head>
-   <title>Calcular edad</title>
- 
-   <script type="text/javascript" language="javascript">
-     function cambiar(){
-       var year = document.getElementById('year').value;
-       var year_serv = <?php echo date('Y')?>;
- 
-       var edad = year_serv-year-1;
- 
-       if(document.getElementById('mes').value==<?php echo date('m')?>){
-         if(document.getElementById('dia').value<=<?php echo date('d')?>){
-           edad += 1;
-          }
-       }else if(document.getElementById('mes').value<=<?php echo date('m')?>){
-         edad += 1;
-       }
- 
-       document.getElementById('edad').value = edad;
-     }
-   </script>
- 
+	<meta charset="UTF-8">
+	<title>Reto Restar Fechas (PHP)</title>
 </head>
- 
 <body>
-<br/><br/>
-   D&iacute;a: <select id="dia" onChange="cambiar()">
-      <?php 
-        for($i=1; $i<32; $i++){
-          echo "<option value='".$i."'>".$i."</option>\n";
-        }
-      ?>
-   </select>
-<br/>
-   Mes: <select id="mes" onChange="cambiar()">
-      <option value="1">Enero</option>
-      <option value="2">Febrero</option>
-      <option value="3">Marzo</option>
-      <option value="4">Abril</option>
-      <option value="5">Mayo</option>
-      <option value="6">Junio</option>
-      <option value="7">Julio</option>
-      <option value="8">Agosto</option>
-      <option value="9">Septiembre</option>
-      <option value="10">Octubre</option>
-      <option value="11">Noviembre</option>
-      <option value="12">Diciembre</option>
-   </select>
-<br/>
-   A&ntilde;o: <select id="year" onChange="cambiar()">
-      <?php 
-        for($i=1950; $i<2012; $i++){
-          echo "<option value='".$i."'>".$i."</option>\n";
-        }
-      ?>
-   </select>
-<br/>
-<br/>
-Tu edad es: <input type="text" id="edad" />
+	<h1>Reto Restar Fechas (PHP)</h1>
+	<hr>
+	<form action="" method="post">
+		<fieldset>
+		<legend><h4>Fecha de Nacimiento</h4></legend>
+		<select name="dia">
+			<option value="">Dia</option>
+			<?php  
+			for ($i=1; $i <= 31 ; $i++) { 
+				echo "<option value'".$i."'>".$i."</option>";
+			}
+			?>
+		</select>
+		<select name="mes">
+			<option value="">Mes</option>
+			<option value="01">Enero</option>
+			<option value="02">Febrero</option>
+			<option value="03">Marzo</option>
+			<option value="04">Abril</option>
+			<option value="05">Mayo</option>
+			<option value="06">Junio</option>
+			<option value="07">Julio</option>
+			<option value="08">Agosto</option>
+			<option value="09">Septiembre</option>
+			<option value="10">Octubre</option>
+			<option value="11">Noviembre</option>
+			<option value="12">Diciembre</option>
+		</select>
+		<select name="ano">
+			<option value="">Año</option>
+			<?php 
+				for ($i=1980; $i < 1997 ; $i++) { 
+					echo "<option value'".$i."'>".$i."</option>";
+				}
+			?>
+		</select>
+		<br>
+		<input type="submit" value="Calcular">
+	</fieldset>
+	</form>
+	<?php 
+		if($_POST) {
+			$dia = $_POST['dia'];
+			$mes = $_POST['mes'];
+			$ano = $_POST['ano'];
+
+			$fecha_nacimiento = mktime(0,0,0, $mes, $dia, $ano);
+			$fecha_actual     = mktime(0,0,0, date('m'), date('d'), date('Y'));
+
+			$fecha_total = $fecha_actual - $fecha_nacimiento;
+			$fecha_dias  = ($fecha_total/60/60/24);
+			$fecha_anos  = ($fecha_total/60/60/24/365);
+			echo "<h4>".floor($fecha_dias)." Días</h4>";
+			echo "<h4>".floor($fecha_anos)." Años</h4>";
+		}
+
+	?>
 </body>
 </html>
